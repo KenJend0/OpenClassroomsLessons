30e5aca302265db0f667fa128be29e205c18a679
KenJend0 <teyssir.aissi@edu.devinci.fr>
2025-10-31 22:21:21 +0100

    Game works with words

---
+++ JavaScript/cours1/config.js
@@ -0,0 +1,8 @@
+    const listeMots = ["javascript", "ordinateur", "programmation", "developpeur", "interface"]
+    /*const listePhrases = [
+        "Le chat noir dort sur le canapé.",
+        "La programmation est une compétence précieuse.",
+        "J'aime apprendre de nouvelles technologies.",
+        "Le soleil brille aujourd'hui.",
+        "Les oiseaux chantent dans les arbres."
+    ]*/
